dccf6a7ca2cda2cce317faa6aa02f951ab8d3564
Brian Tran <tranbrian3@gmail.com>
2025-08-20 22:32:45 -0700

    wip

--- review.py
+++ review.py
@@ -6,10 +6,9 @@ import os
 import sys
 
 project_name = "naurffxiv"
-repo_name = "naurffxiv" # TODO: get current repo
+repo_name = "naurffxiv"  # TODO: get current repo
 git_pulls_api = "https://api.github.com/repos/{0}/{1}/pulls".format(
-    project_name,
-    repo_name
+    project_name, repo_name
 )
 
 
@@ -17,7 +16,7 @@ def create_pull_request(user, head_branch, base_branch, git_token):
     """Creates the pull request for the head_branch against the base_branch"""
     headers = {
         "Authorization": "token {0}".format(git_token),
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     title, description, sha = get_commit_message()
@@ -37,7 +36,12 @@ def create_pull_request(user, head_branch, base_branch, git_token):
     if r.status_code == 422:
         return False
 
-    if subprocess.check_output(["git", "log", f"origin/{base_branch}..HEAD"]).decode("utf-8") == "":
+    if (
+        subprocess.check_output(["git", "log", f"origin/{base_branch}..HEAD"]).decode(
+            "utf-8"
+        )
+        == ""
+    ):
         print("No changes detected")
         return
 
@@ -48,10 +52,7 @@ def create_pull_request(user, head_branch, base_branch, git_token):
         "base": base_branch,
     }
 
-    r = requests.post(
-        git_pulls_api,
-        headers=headers,
-        data=json.dumps(payload))
+    r = requests.post(git_pulls_api, headers=headers, data=json.dumps(payload))
 
     if not r.ok:
         print(f"Pull request updated for branch {head_branch}")
@@ -60,38 +61,42 @@ def create_pull_request(user, head_branch, base_branch, git_token):
     pull_request_result = r.json()
 
     # assign the current user to the pull request
-    git_assignee_api = "https://api.github.com/repos/{0}/{1}/issues/{2}/assignees".format(
-        project_name,
-        repo_name,
-        str(pull_request_result["number"]),
+    git_assignee_api = (
+        "https://api.github.com/repos/{0}/{1}/issues/{2}/assignees".format(
+            project_name,
+            repo_name,
+            str(pull_request_result["number"]),
+        )
     )
 
     data = {
         "assignees": [user],
     }
-    r = requests.post(
-        git_assignee_api,
-        headers=headers,
-        data=json.dumps(data)
-    )
+    r = requests.post(git_assignee_api, headers=headers, data=json.dumps(data))
     return True
+
 
 def update_pull_request():
     subprocess.check_output(["git", "push", "--force", "origin", "HEAD"])
 
-def get_git_branch(path = None):
-    '''Returns the git branch the user is on.'''
+
+def get_git_branch(path=None):
+    """Returns the git branch the user is on."""
     if path is None:
         path = os.path.curdir
-    command = 'git rev-parse --abbrev-ref HEAD'.split()
+    command = "git rev-parse --abbrev-ref HEAD".split()
     branch = subprocess.Popen(command, stdout=subprocess.PIPE, cwd=path).stdout.read()
-    return branch.strip().decode('utf-8')
+    return branch.strip().decode("utf-8")
 
 
 def get_commit_message():
-    '''Parses the first local commit message that have not been pushed remotely yet.'''
+    """Parses the first local commit message that have not been pushed remotely yet."""
     # TODO: replace hardcoded dev with target branch
-    res = subprocess.check_output(["git", "log", "--reverse", "origin/dev..HEAD"]).decode("utf-8").split("\n")
+    res = (
+        subprocess.check_output(["git", "log", "--reverse", "origin/dev..HEAD"])
+        .decode("utf-8")
+        .split("\n")
+    )
     title = ""
     description = ""
     sha = res[0].split()[1]  # assumes the first line is: "commit <hash>"
@@ -111,20 +116,24 @@ def get_commit_message():
 
     return title, description, sha
 
+
 def create_new_branch(new_branch, target_branch):
     try:
         subprocess.check_output(["git", "checkout", "-b", new_branch, target_branch])
         subprocess.check_output(["git", "pull", "--rebase", "origin", target_branch])
         subprocess.check_output(["git", "push", "--set-upstream", "origin", new_branch])
-        subprocess.check_output(["git", "branch", f"--set-upstream-to=origin/{target_branch}", new_branch])
+        subprocess.check_output(
+            ["git", "branch", f"--set-upstream-to=origin/{target_branch}", new_branch]
+        )
     except:
         print("ERROR: cannot create new branch.")
         return
 
+
 def config(git_username, access_token):
     headers = {
         "Authorization": "token {0}".format(access_token),
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     git_find_user = "https://api.github.com/users/{0}".format(git_username)
@@ -168,10 +177,10 @@ if __name__ == "__main__":
             print("No config file found. Run: ./review config")
 
         is_new_pull_request = create_pull_request(
-            conf[0], # current user
-            get_git_branch(), # head_branch
-            "dev", # base_branch TODO: change to automatically detect base branch
-            conf[1], # git_token
+            conf[0],  # current user
+            get_git_branch(),  # head_branch
+            "dev",  # base_branch TODO: change to automatically detect base branch
+            conf[1],  # git_token
         )
         if not is_new_pull_request:
             update_pull_request()
